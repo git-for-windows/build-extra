b14af017151c9cf4f20ad7b3d1683605e5a8dcdc
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-12 19:36:04 +0000

    Let Git completions work with MinGW *and* MSys2 Git
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -9,7 +9,9 @@ PS1="$PS1"'\w'                 # current working directory
 if test -z "$WINELOADERNOEXEC"
 then
 	GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
-	COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}/share/git/completion"
+	COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+	COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+	COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
 	if test -f "$COMPLETION_PATH/git-prompt.sh"
 	then
 		. "$COMPLETION_PATH/git-completion.bash"
