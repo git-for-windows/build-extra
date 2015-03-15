136d67160d216125ff5c619eceb2f7cd364d1b16
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-15 14:06:20 +0100

    Copy-edit msysGit's net installer setup script
    It looks quite different now, actually more like a rewrite, because
    
    - we use Pacman instead of Git to keep our development environment
      up-to-date these days.
    
    - we dropped Git Cheetah (too many problems, too few developers).
    
    - we want to generate our HTML documentation ourselves, now that we have
      a working asciidoc.
    
    Unfortunately, this commit also drops support for HTTP proxies for now:
    It is unclear how Pacman deals with proxies so far. Most likely, it
    heeds the `http_proxy` variable just the same as Git does (because both
    rely on cURL to do the actual job); This needs work by a contributor
    who is behind an HTTP proxy.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ net-installer/setup-git-sdk.bat
@@ -0,0 +1,42 @@
+@REM Set up the Git SDK
+
+@REM determine root directory
+
+@REM https://technet.microsoft.com/en-us/library/bb490909.aspx says:
+@REM <percent>~dpI Expands <percent>I to a drive letter and path only.
+@REM <percent>~fI Expands <percent>I to a fully qualified path name.
+@FOR /F "delims=" %%D in ("%~dp0") do @set cwd=%%~fD
+
+@REM set PATH
+@set PATH=%cwd%\usr\bin;%PATH%
+
+@REM set MSYSTEM so that MSys2 starts up in the correct mode
+@set MSYSTEM=MINGW@@BITNESS@@
+
+@REM update the Pacman package indices first, then force-install msys2-runtime
+@REM (we ship with a stripped-down msys2-runtime, gpg and pacman), so that
+@REM pacman's post-install scripts run without complaining about heap problems
+@%cwd%\usr\bin\pacman -Sy --force --noconfirm msys2-runtime
+
+@REM next, force update pacman
+@%cwd%\usr\bin\pacman -S --force --noconfirm pacman
+
+@REM now update the rest
+@%cwd%\usr\bin\pacman -S --force --noconfirm ^
+	base python less openssh patch make tar diffutils ca-certificates ^
+	perl-Error perl perl-Authen-SASL perl-libwww perl-MIME-tools ^
+	perl-Net-SMTP-SSL perl-TermReadKey ^
+	mintty git-extra ^
+	mingw-w64-@@ARCH@@-git mingw-w64-@@ARCH@@-toolchain ^
+	mingw-w64-@@ARCH@@-curl mingw-w64-@@ARCH@@-expat ^
+	mingw-w64-@@ARCH@@-openssl mingw-w64-@@ARCH@@-tcl ^
+	mingw-w64-@@ARCH@@-pcre
+
+@REM Avoid overlapping address ranges
+@IF MINGW32 == %MSYSTEM% ECHO "Auto-rebasing .dll files"
+@IF MINGW32 == %MSYSTEM% CALL %cwd%\autorebase.bat
+
+@REM now clone the Git sources, build it, and start an interactive shell
+@mintty bash --login -c "mkdir -p /usr/src && cd /usr/src && git -c core.autocrlf=false clone -b master https://github.com/git-for-windows/git && cd git && git config core.autocrlf false && make install; bash -i"
+
+@IF NOT ERRORLEVEL 0 bash --login -i
