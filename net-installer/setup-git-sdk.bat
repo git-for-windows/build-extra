43208428694e9ab8c4b3a1a26b202aefb71bbfcc
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-21 17:02:07 +0100

    net-installer: try harder to install packages
    SourceForge -- which backs MSys2's own packages -- could be a little
    more reliable, meaning that Pacman sometimes errors out because packages
    could either not be downloaded at all, or downloads got interrupted, or
    just got too slow.
    
    To work around this issue, let's try up to five times to install
    packages, and if it still did not work, notify the user and give up.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- net-installer/setup-git-sdk.bat
+++ net-installer/setup-git-sdk.bat
@@ -13,13 +13,44 @@
 @REM set MSYSTEM so that MSys2 starts up in the correct mode
 @set MSYSTEM=MINGW@@BITNESS@@
 
+@SET /A counter=0
+:INSTALL_RUNTIME
+@SET /A counter+=1
+@IF %counter% GEQ 5 (
+	@ECHO "Could not install msys2-runtime"
+	@PAUSE
+	@EXIT 1
+)
+
 @REM update the Pacman package indices first, then force-install msys2-runtime
 @REM (we ship with a stripped-down msys2-runtime, gpg and pacman), so that
 @REM pacman's post-install scripts run without complaining about heap problems
 @%cwd%\usr\bin\pacman -Sy --force --noconfirm msys2-runtime
 
+@IF ERRORLEVEL 1 GOTO INSTALL_RUNTIME
+
+@SET /A counter=0
+:INSTALL_PACMAN
+@SET /A counter+=1
+@IF %counter% GEQ 5 (
+	@ECHO "Could not install pacman"
+	@PAUSE
+	@EXIT 1
+)
+
 @REM next, force update pacman
 @%cwd%\usr\bin\pacman -S --force --noconfirm pacman
+
+@IF ERRORLEVEL 1 GOTO INSTALL_PACMAN
+
+@SET /A counter=0
+:INSTALL_REST
+@SET /A counter+=1
+@IF %counter% GEQ 5 (
+	@ECHO "Could not install the remaining packages"
+	@PAUSE
+	@EXIT 1
+)
 
 @REM now update the rest
 @%cwd%\usr\bin\pacman -S --force --noconfirm ^
@@ -31,6 +62,8 @@
 	mingw-w64-@@ARCH@@-curl mingw-w64-@@ARCH@@-expat ^
 	mingw-w64-@@ARCH@@-openssl mingw-w64-@@ARCH@@-tcl ^
 	mingw-w64-@@ARCH@@-pcre
+
+@IF ERRORLEVEL 1 GOTO INSTALL_REST
 
 @REM Avoid overlapping address ranges
 @IF MINGW32 == %MSYSTEM% (
