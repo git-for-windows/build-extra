12064635a8885284007886f4b32e2e165c305111
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-30 18:06:32 +0200

    installer: adapt the script to install the newest InnoSetup
    This script is so heavily edited that it almost looks like a rewrite,
    but it still is essentially the script written by Sebastian Schuberth.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- installer/update-inno-setup.sh
+++ installer/update-inno-setup.sh
@@ -1,48 +1,35 @@
 #!/bin/sh
 
-# Find a suitable download tool.
-if [ -f "$(type -p curl)" ]; then
-    download="curl"
-    download_args_rss="-s"
-    download_args="-# -L -o"
-elif [ -f "$(type -p wget)" ]; then
-    download="wget"
-    download_args_rss="-q -O -"
-    download_args="-N -O"
-else
-    echo "ERROR: No suitable download tool found."
-    exit 1
-fi
-
-SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
-MSYSGITROOT="$(cd $SCRIPTDIR/../../ && pwd | sed 's/\/$//')/."
-cd $MSYSGITROOT
-
 # Download the most recent Inno Setup version.
-file="is-unicode.exe"
-url="http://www.jrsoftware.org/download.php/$file"
+installer="is-unicode.exe"
+url="http://www.jrsoftware.org/download.php/$installer"
 
-if [ "$download" == "curl" ]; then
-    download_args="$download_args $file -R -z"
-fi
-$download $download_args $file $url
+die () {
+	echo "$*" >&2
+	exit 1
+}
 
-if [ -f $file ]; then
-    # Remove any previous installation.
-    rm -fr share/InnoSetup/
+cd "$(dirname "$0")" ||
+die "Could not switch directory"
 
-    # Silently install Inno Setup.
-    wine=$(type -p wine)
-    if [ $? -eq 0 ]; then
-        $wine $file /verysilent /dir="share\InnoSetup" /noicons /tasks="" /portable=1
-    else
-        # See http://www.mingw.org/wiki/Posix_path_conversion.
-        $file //verysilent //dir="share\InnoSetup" //noicons //tasks="" //portable=1
-    fi
+curl -# -L -O -R -z $installer "$url" ||
+die "Could not download $url"
 
-    # Remove unneeded files from the installation.
-    ( cd share/InnoSetup/ && rm -fr Examples/ Compil32.exe isscint.dll )
+# Remove any previous installation.
+test ! -d InnoSetup ||
+rm -r InnoSetup/ ||
+die "Could not remove previous installation"
 
-    # Clean up.
-    rm $file
-fi
+# Silently install Inno Setup below the mingw root.
+if type wine > /dev/null 2>&1
+then
+	wine $installer /verysilent /dir=InnoSetup /noicons /tasks= /portable=1
+else
+	# See http://www.mingw.org/wiki/Posix_path_conversion.
+	./$installer //verysilent //dir=InnoSetup \
+		//noicons //tasks= //portable=1
+fi ||
+die "Could not install InnoSetup"
+
+# Remove unneeded files from the installation.
+rm -r InnoSetup/Examples/ InnoSetup/Compil32.exe InnoSetup/isscint.dll
