361c74a8e99e41a166ee0c42e6ef96a593b5c9a9
nalla <nalla@hamal.uberspace.de>
2015-04-01 12:10:41 +0100

    installer: port to *MSYS2*
    This is a rough port of the inno setup files to the current *MSYS2*
    environment. The source files are generated by the `release.sh` into
    `file-list.iss`. That file is then included by adding the line
    `#include "file-list.iss"` into `install.iss.in`.
    
    The git-cheeta plugin is commented out temporary.
    
    Signed-off-by: nalla <nalla@hamal.uberspace.de>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- installer/release.sh
+++ installer/release.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+die () {
+	echo "$*" >&2
+	exit 1
+}
+
 force=
-do_compile=t
 while test $# -gt 0
 do
 	case "$1" in
@@ -9,164 +13,78 @@ do
 		force=t
 		shift
 		;;
-	-n|--no-compile)
-		do_compile=
-		shift
-		;;
 	*)
 		break
 	esac
 done
 
-test -z "$1" && {
-	echo "Usage: $0 [-f] [-n] <version>" >&2
-	exit 1
-}
+test $# -gt 0 ||
+die "Usage: $0 [-f] <version>"
 
 version=$1
 
-# change directory to msysGit root
-SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
-MSYSGITROOT="$(cd $SCRIPTDIR/../../ && pwd | sed 's/\/$//')/."
-cd $MSYSGITROOT || {
-	echo "Could not change directory to msysGit root" >&2
-	exit 1
-}
+# change directory to the script's directory
+cd "$(dirname "$0")" ||
+die "Could not switch directory"
 
-test -z "$force" && {
-	die () {
-		echo "$*" >&2
-		echo "If that is okay, please call '$0 -f $version'" >&2
-		exit 1
-	}
+# Export paths to inno setup file
+SCRIPTDIR="$(pwd -W)"
+ROOTDIR="$(cd / && pwd -W)"
+export SCRIPTDIR ROOTDIR
 
-	(cd git &&
-	 git update-index --refresh &&
-	 git diff-files --quiet &&
-	 git diff-index --cached HEAD --) ||
-	die "Git submodule has dirty files"
-	(git update-index --refresh &&
-	 git diff-files --quiet &&
-	 git diff-index --cached HEAD --) ||
-	die "msysGit super project not up-to-date"
-}
+# Evaluate architecture
+ARCH="$(uname -m)"
 
-create_msysgit_tag () {
-	if tag=$(git describe --exact-match --match "*.msysgit.*" HEAD 2> /dev/null)
-	then
-		echo "Using existing tag $tag"
-	else
-		i=0 &&
-		tag=$(git describe HEAD | cut -d- -f1) &&
-		tag=${tag%.msysgit.*} &&
-		while ! git tag -a -m "Git for Windows $1" $tag.msysgit.$i 2> /dev/null
+case "$ARCH" in
+i686)
+	BITNESS=32
+	;;
+x86_64)
+	BITNESS=64
+	;;
+*)
+	die "Unhandled architecture: $ARCH"
+	;;
+esac
+
+# Generate list of files to include
+pacman_list () {
+	pacman -Ql $(for arg
 		do
-			i=$(($i+1))
-		done &&
-		echo "Created tag $tag.msysgit.$i"
-	fi
+			pactree -u "$arg"
+		done |
+		sort |
+		uniq) |
+	grep -v '/$' |
+	sed 's/^[^ ]* //'
 }
 
-# compile everything needed for standard setup
-test "$do_compile" && {
-	wordpad share/WinGit/ReleaseNotes.rtf && {
-		( # create a commit if ReleaseNotes changed
-		 if test ! -z "$(git diff share/WinGit/ReleaseNotes.rtf)"
-		 then
-			git add share/WinGit/ReleaseNotes.rtf &&
-			git commit -m "Git for Windows $version"
-		 fi) &&
-		(cd git &&
-		 create_msysgit_tag $version &&
-		 make install) &&
-		(cd git/contrib/subtree &&
-			make install INSTALL=/bin/install prefix=) &&
-		(cd git/contrib/credential/wincred &&
-			make install INSTALL=/bin/install prefix=) &&
-		/src/mingw-w64/release-easy.sh &&
-		/src/mingw-w64/release-zlib.sh &&
-		(cd src/git-cheetah/explorer/ &&
-		 make objects-clean && make &&
-		 make objects-clean && make W64=1)
-	} || exit 1
-}
+LIST="$(pacman_list mingw-w64-$ARCH-git git-extra ncurses mintty vim \
+	sed awk less grep gnupg findutils coreutils \
+	dos2unix which subversion |
+	grep -v -e '\.[acho]$' -e '/aclocal/' \
+		-e '/man/' \
+		-e '^/usr/include/' -e '^/mingw32/include/' \
+		-e '^/usr/share/doc/' -e '^/mingw32/share/doc/' \
+		-e '^/usr/share/info/' -e '^/mingw32/share/info/' |
+	sed 's/^\///')"
 
-test -z "$force" && {
-	die () {
-		echo "$*" >&2
-		echo "If that is okay, please call '$0 -f $version'" >&2
-		exit 1
-	}
+LIST="$LIST etc/profile etc/bash.bash_logout etc/bash.bashrc etc/fstab"
+LIST="$LIST mingw$BITNESS/etc/gitconfig"
 
-	(cd git &&
-	 git update-index --refresh &&
-	 git diff-files --quiet &&
-	 git diff-index --cached HEAD --) ||
-	die "Git submodule has dirty files"
-	(cd git &&
-	 test git.exe = $((printf 'git.exe\0'; git ls-files -z) | xargs --null ls -t 2>/dev/null| head -1)) ||
-	die "Git's git.exe is not up-to-date (run 'cd /git && make' to fix)"
-	for f in bin/git* libexec/git-core/git*
-	do
-		case "$f" in
-		*.manifest)
-			continue
-			;;
-		*/gitk)
-			basename=gitk-git/gitk
-			;;
-		*/git-citool|*/git-gui)
-			basename=git-gui/git-gui
-			;;
-		*/git-gui--askpass|*/git-gui--askyesno|*/git-gui.tcl)
-			basename=git-gui/$(basename "$f")
-			;;
-		*/git-subtree)
-			basename=contrib/subtree/$(basename "$f")
-			;;
-		*/git-credential-wincred.exe)
-			basename=contrib/credential/wincred/$(basename "$f")
-			;;
-		*)
-			basename=$(basename "$f")
-			;;
-		esac
-		cmp "$f" "git/$basename" ||
-		die "Installed Git disagrees with contents of /git/ ($f)"
-	done
-	(git update-index --refresh &&
-	 git diff-files --quiet &&
-	 git diff-index --cached HEAD --) ||
-	die "msysGit super project not up-to-date"
-	(cd git &&
-	 test ! -z "$(git tag --contains HEAD)") ||
-	die "Git's HEAD is untagged"
-}
+rm -rf file-list.iss
+for f in $LIST
+do
+	printf 'Source: %s; DestDir: {app}\%s; Flags: %s; AfterInstall: %s\n' \
+                $f $(dirname $f) replacesameversion DeleteFromVirtualStore \
+        >> file-list.iss
+done
 
-TMPDIR=/tmp/WinGit
-unset DONT_REMOVE_BUILTINS
+sed -e "s|%APPVERSION%|$version|" -e "s|%MINGW_BITNESS%|mingw$BITNESS|" < install.iss.in > install.iss ||
+exit
 
-$MSYSGITROOT/share/WinGit/copy-files.sh $TMPDIR &&
-sed -e '/share\/msysGit/d' -e "s/msysGit/Git (version $version)/" \
-	< etc/motd > $TMPDIR/etc/motd &&
-cp share/resources/gpl-2.0.rtf share/resources/git.bmp share/resources/gitsmall.bmp $TMPDIR &&
-sed -e "s/%APPVERSION%/$version/" \
-	< share/WinGit/install.iss > $TMPDIR/install.iss &&
-cp share/WinGit/*.inc.iss $TMPDIR &&
 echo "Launching Inno Setup compiler ..." &&
-(cd $TMPDIR &&
- if test -x $MSYSGITROOT/share/InnoSetup/ISCC.exe
- then
-	 $MSYSGITROOT/share/InnoSetup/ISCC.exe install.iss
- else
-	 case $(wine --version) in
-	 wine-0*|wine-1.[012]*|wine-1.3.[0-9]|wine-1.3.[0-9]-*|wine-1.3.1[0-4]|wine-1.3.1[0-4]-*)
-		echo "You need at least WINE version 1.3.15" >&2 &&
-		exit 1
-	 esac &&
-	 wine $MSYSGITROOT/share/InnoSetup/ISCC.exe install.iss
- fi > /tmp/install.out &&
- echo $? > /tmp/install.status) &&
-(test 0 = "$(cat /tmp/install.status)") &&
+./InnoSetup/ISCC.exe install.iss > install.out ||
+die "Could not make installer"
 git tag -a -m "Git for Windows $1" Git-$1 &&
-echo "Installer is available as $(tail -n 1 /tmp/install.out)"
+echo "Installer is available as $(tail -n 1 install.out)"
