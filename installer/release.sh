git-for-windows/build-extra

commit 93f4b05ca806e71021a86f66923f52670393d67b

installer: Do not print "; List of files" twice into file-list.iss
Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

installer/release.sh
@@ -85,8 +85,7 @@ printf "; List of files\n%s\n" \
 >file-list.iss ||
 die "Could not write to file-list.iss"
 
-printf "; List of files\n%s\n" \
-	"Source: \"$SCRIPTDIR\\usr\\share\\git\\ReleaseNotes.css\"; DestDir: {app}\\usr\\share\\git; Flags: replacesameversion; AfterInstall: DeleteFromVirtualStore" \
+printf "Source: \"$SCRIPTDIR\\usr\\share\\git\\ReleaseNotes.css\"; DestDir: {app}\\usr\\share\\git; Flags: replacesameversion; AfterInstall: DeleteFromVirtualStore" \
 >>file-list.iss ||
 die "Could not write to file-list.iss"
 

commit c3ef329d19cbeb5896b12f5bc9911805845be291

Fix line breaks / quoting in file-list.iss
This was broken by 93f4b05.

Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

installer/release.sh
@@ -80,13 +80,10 @@ LIST="$(ARCH=$ARCH BITNESS=$BITNESS PACKAGE_VERSIONS_FILE=package-versions.txt \
 	sh "$SCRIPTDIR"/../make-file-list.sh)" ||
 die "Could not generate file list"
 
-printf "; List of files\n%s\n" \
+printf "; List of files\n%s\n%s\n" \
 	"Source: \"$SCRIPTDIR\\package-versions.txt\"; DestDir: {app}\\etc\\package-versions.txt; Flags: replacesameversion; AfterInstall: DeleteFromVirtualStore" \
+	"Source: \"$SCRIPTDIR\\usr\\share\\git\\ReleaseNotes.css\"; DestDir: {app}\\usr\\share\\git; Flags: replacesameversion; AfterInstall: DeleteFromVirtualStore" \
 >file-list.iss ||
-die "Could not write to file-list.iss"
-
-printf "Source: \"$SCRIPTDIR\\usr\\share\\git\\ReleaseNotes.css\"; DestDir: {app}\\usr\\share\\git; Flags: replacesameversion; AfterInstall: DeleteFromVirtualStore" \
->>file-list.iss ||
 die "Could not write to file-list.iss"
 
 echo "$LIST" |