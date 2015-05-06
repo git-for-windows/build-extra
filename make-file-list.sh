fd0095c0ea3bdf141d57c563b84c8e8bb0f9219b
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-05-06 08:54:59 +0200

    installer/portable: include the `start` script
    It was pointed out by Orgad Shaneh that the `start` script is not part
    of the end users' Git for Windows installation.
    
    This script is part of the `filesystem` package. As that package
    contains files we do not want to ship with Git for Windows, we pluck
    just the `start` script out of it.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- make-file-list.sh
+++ make-file-list.sh
@@ -56,6 +56,7 @@ mingw$BITNESS/etc/gitconfig
 etc/post-install/01-devices.post
 etc/post-install/03-mtab.post
 etc/post-install/06-windows-files.post
+usr/bin/start
 usr/bin/dash.exe
 usr/bin/rebase.exe
 usr/bin/rebaseall
