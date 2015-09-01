d544d974f4e7d86e880177274eb71f267d431a81
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-09-01 10:08:37 +0200

    download-stats: update to 2.5.1
    Courtesy of the newly-introduced `--update` option.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- download-stats.sh
+++ download-stats.sh
@@ -18,29 +18,29 @@ test "--update" != "$1" || {
 }
 
 # IDs
-# 2.3.4.windows.2
+# v2.3.4.windows.2
 #id=${1:-1093748}
-# 2.3.5.windows.4
+# v2.3.5.windows.4
 #id=${1:-1130398}
-# 2.3.5.windows.6
+# v2.3.5.windows.6
 #id=${1:-1133929}
-# 2.3.5.windows.7
+# v2.3.5.windows.7
 #id=${1:-1147969}
-# 2.3.5.windows.8
+# v2.3.5.windows.8
 #id=${1:-1148462}
-# 2.3.6.windows.2
+# v2.3.6.windows.2
 #id=${1:-1215956}
-# 2.3.7.windows.1
+# v2.3.7.windows.1
 #id=${1:-1235013}
-# 2.4.0.windows.1
+# v2.4.0.windows.1
 #id=${1:-1257687}
-# 2.4.0.windows.2
+# v2.4.0.windows.2
 #id=${1:-1272221}
-# 2.4.1.windows.1
+# v2.4.1.windows.1
 #id=${1:-1296332}
-# 2.4.2.windows.1
+# v2.4.2.windows.1
 #id=${1:-1345088}
-# 2.4.3.windows.1
+# v2.4.3.windows.1
 #id=${1:-1409345}
 # v2.4.4.windows.2
 #id=${1:-1441039}
@@ -48,8 +48,10 @@ test "--update" != "$1" || {
 #id=${1:-1471836}
 # v2.4.6.windows.1
 #id=${1:-1554860}
-# 2.5.0.windows.1
-id=${1:-1683962}
+# v2.5.0.windows.1
+#id=${1:-1683962}
+# v2.5.1.windows.1
+id=${1:-1744098}
 
 curl -s https://api.github.com/repos/git-for-windows/git/releases/$id/assets |
 grep -e '"name":' -e '"download_count":'
