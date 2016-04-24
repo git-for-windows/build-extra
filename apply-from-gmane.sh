7a89509f1d99218ca2a10cdce348c66b8dd2549f
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-04-24 17:49:21 +0200

    apply-from-gmane: apply patch series automagically
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- apply-from-gmane.sh
+++ apply-from-gmane.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+die () {
+	echo "$*" >&2
+	exit 1
+}
+
 for URL
 do
 	case "$URL" in
@@ -36,5 +41,36 @@ do
 		;;
 	esac
 
-	curl $URL | git am --whitespace=fix -3 -s || break
+	OUT="$(git rev-parse --git-dir)"/gmane
+	curl -f -o "$OUT" $URL ||
+	die "Could not retrieve $URL" >&2
+
+	if grep '\[PATCH.* 0/[1-9]' "$OUT"
+	then
+		echo "Multi-part: $OUT $URL" >&2
+		GROUP=${URL%/raw}
+		THREAD=${GROUP##*/}
+		GROUP=${GROUP%/$THREAD}
+		GROUP=${GROUP##*/}
+		URL2=http://news.gmane.org/group/$GROUP/thread=$THREAD
+		OUT2="$OUT.coverletter.html"
+		curl -f -o "$OUT2" "$URL2" ||
+		die "Could not retrieve cover letter from $URL2" >&2
+
+		NO=1
+		while true
+		do
+			URL3="$(sed -n '/.*\[PATCH.*'$NO'\/[1-9]/{
+					s/.*\(http:\/\/article[^\"]*\).*/\1/p;q
+				}' <"$OUT2")"
+			test -n "$URL3" || break
+			OUT3="$OUT.$NO"
+			curl -f -o "$OUT3" $URL3/raw ||
+			die "Could not retrieve $URL3" >&2
+			git am --whitespace=fix -3 -s <"$OUT3" || break
+			NO=$(($NO+1))
+		done
+	else
+		git am --whitespace=fix -3 -s <"$OUT" || break
+	fi
 done
