40866168a73175f57df2fbc12186e2b49aed398c
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-08-02 18:13:22 +0200

    Add a script to commit the files of a Git for Windows SDK
    To facilitate spawning build agents, let's have an easy-to-fetch (and
    easy-to-cache) repository containing all the files of a Git for Windows
    SDK.
    
    The concept is similar to the olden days of msysGit (no, no, no, not Git
    for Windows, msysGit was never Git for Windows, it was always the
    development environment for the contributors who were willing to
    develop Git for Windows). In those days, we abused Git as a package
    management system.
    
    The difference is that we have a proper package management system now,
    and we simply synchronize the files into a Git repository so that build
    agents have an easier time to keep up-to-date with the Git for Windows
    SDK (because there is no chance that build agents can update some MSYS2
    installation interactively, working around problems with bash, pacman or
    the msys2-runtime being updated while in use).
    
    This script is not in its final form: currently, it is only able to
    perform an initial commit of all packages. We will have to teach it to
    detect which packages were updated, and to remove the old files.
    
    And then we will also have to teach the script which files are part of a
    package even if they were not included verbatim in the .tar.xz archive
    but generated by the .INSTALL script.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ commit-msys2.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# To help Continuous Integration, this script commits the files installed via
+# MSYS2 packages into the Git for Windows SDK. The resulting repository can be
+# easily cloned and cached by build agents.
+
+die () {
+	echo "$*" >&2
+	exit 1
+}
+
+root="$(cd "$(dirname "$0")/../../.." && pwd)" ||
+die "Could not determine root directory"
+
+import_tars="$root"/usr/src/git/contrib/fast-import/import-tars.perl
+test -x "$import_tars" ||
+die "You need to run this script in a Git for Windows SDK"
+
+list_packages () {
+	(cd "$root"/var/lib/pacman/local &&
+	 ls | grep -v ALPM_DB_VERSION)
+}
+
+commit_package () {
+	(cd "$root" ||
+	 die "Could not cd to $root"
+
+	 git update-index -q --refresh &&
+	 git diff-index --cached --quiet HEAD -- ||
+	 die "There are uncommitted changes in $root"
+
+	 package="$(echo var/cache/pacman/pkg/"$1"*.pkg.tar.xz)"
+
+	 test -f "$package" || die "Could not find the package of $1"
+
+	 if git rev-parse -q --verify refs/heads/import-tars >/dev/null
+	 then
+		git branch -D import-tars
+	 fi &&
+	 "$import_tars" "$package" &&
+	 git ls-tree -r import-tars |
+	 sed -n 's/^\([0-9]* \)blob \([0-9a-f]*\)\(\t[^.].*\)/\1\2\3/p' |
+	 git update-index --index-info &&
+	 git commit -s -m "$1" ||
+	 die "Could not commit $1") ||
+	exit
+}
+
+for package in $(list_packages)
+do
+	commit_package $package
+done
