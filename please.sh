461d3319956da1ed1c0cc3c66ad44e1450fe7b1b
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-11-15 13:43:26 +0100

    please.sh prerelease: ensure that asciidoctor is found
    ... by ensuring that MSYS2 uses the MINGW64 PATH.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- please.sh
+++ please.sh
@@ -1122,6 +1122,7 @@ prerelease () { # [--installer | --portable | --mingit] [--clean-output=<directo
 			extra="SIGNTOOL=\"git --git-dir=\\\"$sdk64/usr/src"
 			extra="$extra/build-extra/.git\\\" signtool\" "
 		fi
+		MSYSTEM=MINGW64 \
 		"$sdk/git-cmd.exe" --command=usr\\bin\\sh.exe -l -c \
 			"cd \"$git_src_dir/../..\" &&"'
 			MAKEFLAGS=-j5 MINGW_INSTALLS=mingw32\ mingw64 \
