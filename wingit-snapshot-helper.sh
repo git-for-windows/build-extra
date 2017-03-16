e53d66006e4ae7ee1f39550fc243cf4632354e5a
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-03-16 23:34:00 +0100

    Example upload to blob storage
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- wingit-snapshot-helper.sh
+++ wingit-snapshot-helper.sh
@@ -15,19 +15,23 @@ access_key="$3"
 blob_store_url="blob.core.windows.net"
 authorization="SharedKey"
 
-request_method="GET"
+request_method="PUT"
 request_date=$(TZ=GMT date "+%a, %d %h %Y %H:%M:%S %Z")
 storage_service_version="2016-05-31"
 
 # HTTP Request headers
+x_ms_blob_type="x-ms-blob-type:BlockBlob"
 x_ms_date_h="x-ms-date:$request_date"
 x_ms_version_h="x-ms-version:$storage_service_version"
 
-# Build the signature string
-canonicalized_headers="$x_ms_date_h\n$x_ms_version_h"
-canonicalized_resource="/$storage_account/$container_name"
+content_length=5603
+content_length_header="Content-Length: $content_length"
 
-string_to_sign="$request_method\n\n\n\n\n\n\n\n\n\n\n\n$canonicalized_headers\n$canonicalized_resource\ncomp:list\nrestype:container"
+# Build the signature string
+canonicalized_headers="$x_ms_blob_type\n$x_ms_date_h\n$x_ms_version_h"
+canonicalized_resource="/$storage_account/$container_name/EXAMPLE.md"
+
+string_to_sign="$request_method\n\n\n$content_length\n\napplication/x-www-form-urlencoded\n\n\n\n\n\n\n$canonicalized_headers\n$canonicalized_resource"
 
 # Decode the Base64 encoded access key, convert to Hex.
 decoded_hex_key="$(printf %s "$access_key" | base64 -d -w0 | xxd -p -c256)"
@@ -38,7 +42,11 @@ authorization_header="Authorization: $authorization $storage_account:$signature"
 
 set -x
 curl -i \
+  -H "$x_ms_blob_type" \
   -H "$x_ms_date_h" \
   -H "$x_ms_version_h" \
+  -H "$content_length_header" \
   -H "$authorization_header" \
-  "https://$storage_account.$blob_store_url/$container_name?restype=container&comp=list"
+  -X$request_method \
+  --data-binary @README.md \
+  "https://$storage_account.$blob_store_url/$container_name/EXAMPLE.md"
