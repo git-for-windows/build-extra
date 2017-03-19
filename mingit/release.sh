34c81e70370af3d57cfbe281716bd9e1d9568f85
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-03-19 12:43:22 +0100

    mingit: use an overlay directory
    Just like in the portable Git script, we now copy stuff into the `root/`
    subdirectory. This step not only unifies the way mingit's script works
    with portable Git's, it also prepares for a more efficient way to
    package mingit.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- mingit/release.sh
+++ mingit/release.sh
@@ -47,15 +47,19 @@ case "$SCRIPT_PATH" in
 	;;
 esac
 
-sed 's/$/\r/' <"$SCRIPT_PATH"/../LICENSE.txt >"$SCRIPT_PATH"/LICENSE.txt ||
+rm -rf "$SCRIPT_PATH"/root &&
+mkdir -p "$SCRIPT_PATH"/root ||
+die "Could not create overlay directory"
+
+sed 's/$/\r/' <"$SCRIPT_PATH"/../LICENSE.txt >"$SCRIPT_PATH"/root/LICENSE.txt ||
 die "Could not copy license file"
 
-mkdir -p "$SCRIPT_PATH"/etc ||
+mkdir -p "$SCRIPT_PATH"/root/etc ||
 die "Could not make etc/"
 
 # Make a list of files to include
 LIST="$(ARCH=$ARCH BITNESS=$BITNESS MINIMAL_GIT=1 \
-	PACKAGE_VERSIONS_FILE="$SCRIPT_PATH"/etc/package-versions.txt \
+	PACKAGE_VERSIONS_FILE="$SCRIPT_PATH"/root/etc/package-versions.txt \
 	sh "$SCRIPT_PATH"/../make-file-list.sh "$@")" ||
 die "Could not generate file list"
 
@@ -72,22 +76,22 @@ grep "^mingw$BITNESS/libexec/git-core/.*\.exe$" |
 sort >"$SCRIPT_PATH"/sorted-libexec-exes &&
 MOVED_FILE=etc/libexec-moved.txt &&
 comm -12 "$SCRIPT_PATH"/sorted-all "$SCRIPT_PATH"/sorted-libexec-exes \
-	>"$SCRIPT_PATH"/$MOVED_FILE &&
-if test ! -s "$SCRIPT_PATH"/$MOVED_FILE
+	>"$SCRIPT_PATH"/root/$MOVED_FILE &&
+if test ! -s "$SCRIPT_PATH"/root/$MOVED_FILE
 then
 	die "Could not find any .exe files in libexec/git-core/"
 fi &&
 BIN_DIR=mingw$BITNESS/bin &&
-rm -rf "$SCRIPT_PATH"/$BIN_DIR &&
-mkdir -p "$SCRIPT_PATH"/$BIN_DIR &&
-(cd / && cp $(cat "$SCRIPT_PATH"/$MOVED_FILE) "$SCRIPT_PATH"/$BIN_DIR/) &&
-LIST="$(comm -23 "$SCRIPT_PATH"/sorted-all "$SCRIPT_PATH"/$MOVED_FILE)" ||
+mkdir -p "$SCRIPT_PATH"/root/$BIN_DIR &&
+(cd / &&
+ cp $(cat "$SCRIPT_PATH"/root/$MOVED_FILE) "$SCRIPT_PATH"/root/$BIN_DIR/) &&
+LIST="$(comm -23 "$SCRIPT_PATH"/sorted-all "$SCRIPT_PATH"/root/$MOVED_FILE)" ||
 die "Could not copy libexec/git-core/*.exe"
 
 test ! -f "$TARGET" || rm "$TARGET" || die "Could not remove $TARGET"
 
 echo "Creating .zip archive" &&
-(cd "$SCRIPT_PATH" &&
+(cd "$SCRIPT_PATH"/root &&
  zip -9r "$TARGET" LICENSE.txt etc/package-versions.txt $MOVED_FILE $BIN_DIR/) &&
 (cd / && zip -9r "$TARGET" $LIST) &&
 echo "Success! You will find the new MinGit at \"$TARGET\"."
