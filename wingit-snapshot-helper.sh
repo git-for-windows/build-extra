883abe8a08f6e7fb82c3236304a0f79124f8d9c1
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-04-03 20:56:56 +0200

    wingit-snapshot-helper: implement different actions
    For now, only 'list' and 'upload' are supported.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- wingit-snapshot-helper.sh
+++ wingit-snapshot-helper.sh
@@ -5,53 +5,101 @@ die () {
 	exit 1
 }
 
-test $# = 4 ||
-die "usage: ${0##*/} <storage-account-name> <container-name> <access-key> <file>"
+test $# -ge 4 ||
+die "usage: ${0##*/} <storage-account-name> <container-name> <access-key> ( list | upload <file>...)"
 
-storage_account="$1"
-container_name="$2"
-access_key="$3"
-file="$4"
-basename="${file##*/}"
+storage_account="$1"; shift
+container_name="$1"; shift
+access_key="$1"; shift
 
-test -f "$file" ||
-die "File does not exist: '$file'"
+req () {
+	file=
+	x_ms_blob_type=
+	content_length=
+	content_length_header=
+	content_type=
+	resource_trailer=
+	get_parameters=
+	string_to_sign_extra=
+	case "$1" in
+	upload)
+		file="$2"
+		test -f "$file" || {
+			echo "File does not exist: '$file'" >&2
+			return 1
+		}
 
-blob_store_url="blob.core.windows.net"
-authorization="SharedKey"
+		resource_extra=/"${file##*/}"
 
-request_method="PUT"
-request_date=$(TZ=GMT date "+%a, %d %h %Y %H:%M:%S %Z")
-storage_service_version="2016-05-31"
+		request_method="PUT"
+		x_ms_blob_type="x-ms-blob-type:BlockBlob"
+		content_length="$(stat -c %s "$file")"
+		content_length_header="Content-Length: $content_length"
+		content_type="application/x-www-form-urlencoded"
+		;;
+	list)
+		request_method="GET"
+		get_parameters="?restype=container&comp=list"
+		string_to_sign_extra="\ncomp:list\nrestype:container"
+		;;
+	*)
+		die "req called with unknown command: $1"
+		;;
+	esac
 
-# HTTP Request headers
-x_ms_blob_type="x-ms-blob-type:BlockBlob"
-x_ms_date_h="x-ms-date:$request_date"
-x_ms_version_h="x-ms-version:$storage_service_version"
+	blob_store_url="blob.core.windows.net"
+	authorization="SharedKey"
 
-content_length="$(stat -c %s "$file")"
-content_length_header="Content-Length: $content_length"
+	request_date=$(TZ=GMT date "+%a, %d %h %Y %H:%M:%S %Z")
+	storage_service_version="2016-05-31"
 
-# Build the signature string
-canonicalized_headers="$x_ms_blob_type\n$x_ms_date_h\n$x_ms_version_h"
-canonicalized_resource="/$storage_account/$container_name/$basename"
+	# HTTP Request headers
+	x_ms_date_h="x-ms-date:$request_date"
+	x_ms_version_h="x-ms-version:$storage_service_version"
 
-string_to_sign="$request_method\n\n\n$content_length\n\napplication/x-www-form-urlencoded\n\n\n\n\n\n\n$canonicalized_headers\n$canonicalized_resource"
+	# Build the signature string
+	canonicalized_headers="${x_ms_blob_type:+$x_ms_blob_type\n}$x_ms_date_h\n$x_ms_version_h"
+	canonicalized_resource="/$storage_account/$container_name$resource_extra"
 
-# Decode the Base64 encoded access key, convert to Hex.
-decoded_hex_key="$(printf %s "$access_key" | base64 -d -w0 | xxd -p -c256)"
+	string_to_sign="$request_method\n\n\n$content_length\n\n$content_type\n\n\n\n\n\n\n$canonicalized_headers\n$canonicalized_resource$string_to_sign_extra"
 
-# Create the HMAC signature for the Authorization header
-signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$decoded_hex_key" -binary |  base64 -w0)
-authorization_header="Authorization: $authorization $storage_account:$signature"
+	# Decode the Base64 encoded access key, convert to Hex.
+	decoded_hex_key="$(printf %s "$access_key" | base64 -d -w0 | xxd -p -c256)"
 
-set -x
-curl -i \
-  -H "$x_ms_blob_type" \
-  -H "$x_ms_date_h" \
-  -H "$x_ms_version_h" \
-  -H "$content_length_header" \
-  -H "$authorization_header" \
-  -X$request_method \
-  --data-binary @"$file" \
-  "https://$storage_account.$blob_store_url/$container_name/$basename"
+	# Create the HMAC signature for the Authorization header
+	signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$decoded_hex_key" -binary |  base64 -w0)
+	authorization_header="Authorization: $authorization $storage_account:$signature"
+
+	{
+		test -z "$x_ms_blob_type" || echo "-H \"$x_ms_blob_type\""
+		echo "-H \"$x_ms_date_h\""
+		echo "-H \"$x_ms_version_h\""
+		test -z "$content_length_header" ||
+		echo "-H \"$content_length_header\""
+		echo "-H \"$authorization_header\""
+		echo "-X$request_method"
+		test -z "$file" || echo "--data-binary \"@$file\""
+	} |
+	curl -i -K - \
+	  "https://$storage_account.$blob_store_url/$container_name$resource_extra$get_parameters"
+}
+
+action="$1"; shift
+case "$action" in
+list)
+	test $# = 0 || die "'list' does not accept arguments"
+	req "$action"
+	;;
+upload)
+	test $# -gt 0 || die "'upload' requires arguments"
+	ret=0
+	for file
+	do
+		req "$action" "$file" || ret=1
+	done
+	exit $ret
+	;;
+*)
+	die "Unhandled action: '$action'"
+	;;
+esac
