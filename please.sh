7bc72efb8ba98fb9185b25d5f7cae11bd630dc42
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-09-18 15:55:43 +0200

    please.sh prerelease: really do not get confused by drive prefixes
    When pushing the tag into the Git repository in Git for Windows SDK's
    /usr/src/MINGW-packages/mingw-w64-git/src/git/ directory, let's just
    turn the path into a pseudo-Unix directory. That way, Git does not even
    see the drive prefix, and hence cannot think the drive letter is a
    protocol specifier.
    
    An earlier attempt to fix this (by prefixing with file:///) did not work
    reliably.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- please.sh
+++ please.sh
@@ -1540,7 +1540,8 @@ prerelease () { # [--installer | --portable | --mingit] [--only-64-bit] [--clean
 		git tag -f -a -m "Prerelease of $1" "$tag_name" "$1" ||
 		die "Could not create tag '%s'\n" "$tag_name"
 
-		git push --force file:///"$git_src_dir" "refs/tags/$tag_name" ||
+		git push --force "$(cygpath -au "$git_src_dir")" \
+			"refs/tags/$tag_name" ||
 		die "Could not push tag '%s' to '%s'\n" \
 			"$tag_name" "$git_src_dir"
 
@@ -1557,7 +1558,8 @@ prerelease () { # [--installer | --portable | --mingit] [--only-64-bit] [--clean
 		git tag -a -m "Prerelease of $1" "$tag_name" "$1" ||
 		die "Could not create tag '%s'\n" "$tag_name"
 
-		git push file:///"$git_src_dir" "refs/tags/$tag_name" ||
+		git push "$(cygpath -au "$git_src_dir")" \
+			"refs/tags/$tag_name" ||
 		die "Could not push tag '%s' to '%s'\n" \
 			"$tag_name" "$git_src_dir"
 	fi
