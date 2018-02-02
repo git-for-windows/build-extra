3b0b5c0f56cffb65af03a5328cd02d4b1b151376
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-02-02 20:54:24 +0000

    download-stats: new Git for Windows version
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- download-stats.sh
+++ download-stats.sh
@@ -18,8 +18,6 @@ test "--update" != "$1" || {
 }
 
 # IDs
-# v2.12.2.windows.2
-#id=${1:-5987210}
 # v2.13.0.windows.1
 #id=${1:-6335523}
 # v2.13.1.windows.1
@@ -77,7 +75,10 @@ test "--update" != "$1" || {
 # v2.16.0.windows.2
 #id=${1:-9296463}
 # v2.16.1.windows.1
-id=${1:-9334485}
+#id=${1:-9334485}
+# v2.16.1.windows.2
+id=${1:-9514737}
+# v2.11.1.mingit-prerelease.2
 # v2.11.1.mingit-prerelease.2
 # v2.11.1.mingit-prerelease.1
 # v2.11.1.mingit-prerelease.2
