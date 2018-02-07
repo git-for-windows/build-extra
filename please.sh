81de404cf8bc56a40901629864b8328613711e39
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-02-07 22:40:25 +0100

    please.sh publish: fix the order of the .pdb archives
    We always try to list the 64-bit versions first. No need to change that.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- please.sh
+++ please.sh
@@ -3562,8 +3562,8 @@ publish () { #
 			MinGit-"$ver"-busybox-32-bit.zip \
 			Git-"$ver"-64-bit.tar.bz2 \
 			Git-"$ver"-32-bit.tar.bz2 \
-			pdbs-for-git-32-bit-$git_pkgver.zip \
-			pdbs-for-git-64-bit-$git_pkgver.zip) |
+			pdbs-for-git-64-bit-$git_pkgver.zip \
+			pdbs-for-git-32-bit-$git_pkgver.zip) |
 		sed -n 's/\([^ ]*\) \*\(.*\)/\2 | \1/p')"
 	body="$(printf "%s\n\n%s" "$text" "$checksums")"
 	quoted="$(echo "$body" |
@@ -3581,8 +3581,8 @@ publish () { #
 		"$HOME"/MinGit-"$ver"-busybox-32-bit.zip \
 		"$HOME"/Git-"$ver"-64-bit.tar.bz2 \
 		"$HOME"/Git-"$ver"-32-bit.tar.bz2 \
-		"$HOME"/pdbs-for-git-32-bit-$git_pkgver.zip \
-		"$HOME"/pdbs-for-git-64-bit-$git_pkgver.zip ||
+		"$HOME"/pdbs-for-git-64-bit-$git_pkgver.zip \
+		"$HOME"/pdbs-for-git-32-bit-$git_pkgver.zip ||
 	die "Could not upload files\n"
 
 	for nupkg in GitForWindows Git-Windows-Minimal
