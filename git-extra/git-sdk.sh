da74fcafc4f0b5fbdc501825d9601adac948b42d
Beat Bolli <dev+git@drbeat.li>
2018-02-27 23:51:32 +0100

    git-extra: provide an "sdk" development helper function
    Let our developers-to-be get started in a less intimidating way.
    See the built-in help (`sdk help`) for its subcommands.
    
    This addresses https://github.com/git-for-windows/git/issues/1357.
    
    Signed-off-by: Beat Bolli <dev+git@drbeat.li>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- git-extra/git-sdk.sh
+++ git-extra/git-sdk.sh
@@ -20,3 +20,54 @@ case "$rootdir" in
 	fi
 	;;
 esac
+
+sdk () {
+	case "$1" in
+	help|--help|-h)
+		cat >&2 <<-EOF
+		The 'sdk' shell function helps you to get up and running
+		with the Git for Windows SDK. The available subcommands are:
+
+		create-desktop-icon: install a desktop icon that starts the GfW SDK shell.
+
+		init <repo>: initialize and/or update a development repo. Known repos
+		    are: build-extra, git, MINGW-packages, MSYS2-packages.
+
+		build-git:   initializes the Git repo and builds Git.
+		EOF
+		;;
+	create-desktop-icon)
+		create-shortcut.exe --icon-file /msys2.ico --work-dir / /git-bash.exe \
+		"$HOME/Desktop/Git SDK$(case "$(uname -m)" in i686) echo " 32-bit";; x86_64) echo " 64-bit";; esac).lnk"
+		;;
+	die)
+		shift
+		echo "$*" >&2
+		return 1
+		;;
+	init)
+		case "$2" in
+		build-extra|git|MINGW-packages|MSYS2-packages)
+			dest="/usr/src/$2"
+			if test ! -d "$dest"/.git
+			then
+				mkdir -p "$dest" &&
+				git init "$dest" &&
+				git -C "$dest" remote add origin https://github.com/git-for-windows/$2
+			fi &&
+			git -C "$dest" pull origin master
+			;;
+		*)
+			sdk die "Unhandled repository: $2" >&2
+			;;
+		esac
+		;;
+	build-git)
+		sdk init git &&
+		make -C /usr/src/git -j$(nproc) DEVELOPER=1
+		;;
+	*)
+		sdk die "Usage: sdk ( build-git | init <repo> | create-desktop-icon | help )"
+		;;
+	esac
+}
