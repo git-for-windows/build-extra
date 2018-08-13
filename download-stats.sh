1ed4c467ced122af1460ea521b9b44f87ec065ce
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-08-13 21:07:44 +0200

    download-stats: fix the --update option
    ... and reinstate the current set of versions
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- download-stats.sh
+++ download-stats.sh
@@ -7,9 +7,10 @@ test "--update" != "$1" || {
 	curl -s \
 	 https://api.github.com/repos/git-for-windows/git/releases |
 	tac |
-	sed -n '/^    "tag_name":/{
-    N;
-    s/.*"tag_name": "\([^"]*\)"[^"]*"id": \([0-9]*\).*/# \1\n#id=${1:-\2}/p
+	sed -n '/^    "\(tag_name\|id\)":/{
+    N;N;
+    y/\n/ /;
+    s/.*"tag_name": "\([^"]*\)".*"id": \([0-9]*\).*/# \1\n#id=${1:-\2}/p
 }' | sed '$s/^#//' >"${0%.sh}".ids &&
 	echo "$ids" |
 	sed -i -e '/^#\( v\?2.*windows\|id=\)/d' -e '/^id=/d' \
@@ -18,6 +19,66 @@ test "--update" != "$1" || {
 }
 
 # IDs
+# v2.15.1.windows.1
+#id=${1:-8700844}
+# v2.15.1.windows.2
+#id=${1:-8710511}
+# v2.13.1.windows.4
+#id=${1:-8759345}
+# v2.13.1.windows.6
+#id=${1:-8925346}
+# v2.13.1.windows.7
+#id=${1:-8928934}
+# v2.16.0-rc0.windows.1
+#id=${1:-9079338}
+# v2.14.3.windows.2
+#id=${1:-9124043}
+# v2.16.0-rc1.windows.1
+#id=${1:-9134153}
+# v2.16.0-rc2.windows.1
+#id=${1:-9220789}
+# v2.16.0.windows.2
+#id=${1:-9296463}
+# v2.16.1.windows.1
+#id=${1:-9334485}
+# v2.16.1.windows.2
+#id=${1:-9514737}
+# v2.16.1.windows.3
+#id=${1:-9555514}
+# v2.16.1.windows.4
+#id=${1:-9579600}
+# v2.16.2.windows.1
+#id=${1:-9749756}
+# v2.17.0-rc0.windows.1
+#id=${1:-10134423}
+# v2.17.0-rc1.windows.1
+#id=${1:-10235214}
+# v2.16.3.windows.1
+#id=${1:-10235771}
+# v2.17.0-rc2.windows.1
+#id=${1:-10322089}
+# v2.17.0.windows.1
+#id=${1:-10369295}
+# v2.17.1.windows.1
+#id=${1:-11224533}
+# v2.17.1.windows.2
+#id=${1:-11227511}
+# v2.14.4.windows.1
+#id=${1:-11238015}
+# v2.11.1.mingit-prerelease.3
+#id=${1:-11238166}
+# v2.18.0-rc1.windows.1
+#id=${1:-11395085}
+# v2.18.0-rc1.windows.2
+#id=${1:-11427247}
+# v2.18.0-rc1.windows.3
+#id=${1:-11448041}
+# v2.18.0-rc2.windows.1
+#id=${1:-11480848}
+# v2.14.4.windows.2
+#id=${1:-11589766}
+# v2.18.0.windows.1
+id=${1:-11604912}
 # v2.11.1.mingit-prerelease.2
 # v2.11.1.mingit-prerelease.2
 # v2.11.1.mingit-prerelease.2
