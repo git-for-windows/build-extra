fe8e3cb4978d222dcfba556221872df59ab8c030
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-02-05 20:41:06 +0100

    ever-green: offer to integrate new changes into the ever-green branch
    The first step of ever-green branches is to update them with regard to
    their original branches.
    
    Example: the ever-green branch shears/pu of Git for Windows reflects its
    `master` (original branch) as if it were rebased to git.git's `pu`
    (upstream branch). Whenever Git for Windows' `master` advances, we will
    want to include those changes in shears/pu. This here commit implements
    that.
    
    Note: for simplicity's sake, this patch rebases cousins. Example:
    
    	master:			shears/pu
    
    	- A - B ---- D		- A' - B'
    	    \      /
    	     --- C
    
    Clearly, A' and B' are already-rebased versions of A and B, and we will
    want to rebase only C and D. Because we rebase cousins with this here
    current patch, the result will look like this:
    
    	- A' - B' ---- D'
    		\    /
    		  C'
    
    This is obviously incorrect, but it is a little tricky to find the
    correct base commit(s) for "cousins". So for simplicity's sake, we keep
    that logic out of the current commit. The next commit will work on that,
    though, so that we will end up with this instead:
    
    	- A' - B' --- D'
    	   \        /
    	    ---- C'
    
    Since we do not *actually* want to rebase the original branch, we only
    generate the todo list as if we wanted to, and then start a rebase of
    the ever-green branch and inject that previously generated todo list
    instead.
    
    After integrating ("cherry-picking", but with merge commits) the new
    changes of the original branch into the ever-green branch, we want to
    rebase result onto the upstream branch if that one changed.
    
    Note that we cannot simply run those two rebases separately: We do
    expect merge conflicts to happen, *in particular* in the new changes of
    the original branch. Meaning that the first rebase might abort with an
    error, the user will fix the merge conflict and continue the rebase, but
    after that rebase finished, the ever-green.sh script is no longer in
    charge and therefore cannot kick off the second rebase.
    
    So we cannot simply run two rebases in this script. To help with that,
    we use a (supported) trick: we inject an `exec` command (calling the
    ever-green.sh script) at the very end of the first rebase, to generate
    the todo list of the second rebase and to append it to the current todo
    list. The interactive rebase will detect that the todo list changed, and
    process the appended commands. That way, both rebases will be combined
    into a single run, and all is dandy.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ever-green.sh
+++ ever-green.sh
@@ -33,6 +33,109 @@ die () {
 	exit 1
 }
 
+make_script () { # <tip-commit-to-rebase> <rebase-i-options>...
+	# Create throw-away worktree in order to generate todo list
+	worktree="$(git rev-parse --absolute-git-dir)/temp-rebase.$$"
+	git worktree add --no-checkout "$worktree" "$1"^0 >&2
+	shift
+
+	cur_git_dir="$(git rev-parse --absolute-git-dir)"
+	tmp_todo_list="$worktree.todo"
+	rm -f "$tmp_todo_list" || die "Could not remove $tmp_todo_list"
+
+	fake_editor="$(git -C "$worktree" rev-parse --absolute-git-dir)/fake-editor.sh" &&
+	cat >"$fake_editor" <<-EOF &&
+	#!/bin/sh
+
+	cat "\$1" >"$tmp_todo_list" &&
+	exit 1
+	EOF
+	chmod a+x "$fake_editor" &&
+
+	if out="$(unset GIT_DIR; GIT_SEQUENCE_EDITOR="$fake_editor" git -C "$worktree" rebase -i "$@" 2>&1)" ||
+		test ! -f "$tmp_todo_list"
+	then
+		die "Failed to generate todo list for 'rebase $*' in $PWD: $out"
+	fi
+
+	git worktree remove --force "$worktree" &&
+	result="$(cat "$tmp_todo_list")" &&
+	rm "$tmp_todo_list" &&
+	echo "$result"
+}
+
+extract_todo_help () {
+	help_starts_at="$(grep -n '^# Rebase .* ([0-9]* commands)' <"$1")"
+	help_starts_at="${help_starts_at%%:*}"
+	case "$help_starts_at" in
+	'') help=;;
+	*[^0-9]*) die "BUG: could not get start of help in $1";;
+	*)
+		mv -f "$1" "$1.raw" ||
+		die "Could not rename $1"
+
+		help="$(sed "1,$(($help_starts_at-2))d" <"$1.raw")"
+		sed "$(($help_starts_at-1))q" >"$1" <"$1.raw"
+		;;
+	esac
+	echo "$help"
+}
+
+case "$1" in
+replace-todo-script)
+	shift
+	cp -f "$(git rev-parse --git-dir)/replace-todo" "$1" ||
+	die "Could not replace todo list"
+
+	eval "\"$ORIGINAL_GIT_SEQUENCE_EDITOR\" \"$1\"" && exit
+	die "Failed to call '$ORIGINAL_GIT_SEQUENCE_EDITOR'"
+	;;
+nested-rebase)
+	shift
+	todo="$(git rev-parse --git-path rebase-merge/git-rebase-todo)" &&
+	help="$(extract_todo_help "$todo")" &&
+	mv -f "$todo" "$todo.save" ||
+	die "Could not save $todo"
+
+	onto="$*"; onto="${onto#*--onto }"
+	test "a$onto" != "a$*" ||
+	die "Could not determine --onto from '$*'"
+	onto="$(git rev-parse "${onto%% *}")" ||
+	die "Invalid onto: '$*'"
+
+	echo "# Now let's rebase the ever-green branch onto the upstream branch" >"$todo" &&
+	echo "reset $onto" >>"$todo" &&
+	make_script HEAD "$@" >>"$todo" ||
+	die "Could not retrieve new todo list"
+
+	help="$(extract_todo_help "$todo")" &&
+	cat "$todo.save" >>"$todo" &&
+	echo "$help" >>"$todo" ||
+	die "Could not append saved todo commands"
+
+	if test -z "$ORIGINAL_GIT_SEQUENCE_EDITOR"
+	then
+		# If ORIGINAL_GIT_SEQUENCE_EDITOR is no longer set, that means
+		# that the rebase was interrupted and restarted, i.e. the
+		# GIT_SEQUENCE_EDITOR is no longer overridden by ever-green.sh.
+		ORIGINAL_GIT_SEQUENCE_EDITOR="$GIT_SEQUENCE_EDITOR"
+		test -n "$ORIGINAL_GIT_SEQUENCE_EDITOR" ||
+		ORIGINAL_GIT_SEQUENCE_EDITOR="$(git config sequence.editor)"
+		test -n "$ORIGINAL_GIT_SEQUENCE_EDITOR" ||
+			ORIGINAL_GIT_SEQUENCE_EDITOR="$(git var GIT_EDITOR)"
+		test -n "$ORIGINAL_GIT_SEQUENCE_EDITOR" ||
+		die "Could not determine editor"
+	fi
+	eval "\"$ORIGINAL_GIT_SEQUENCE_EDITOR\" \"$todo\"" ||
+	die "Could not launch $ORIGINAL_GIT_SEQUENCE_EDITOR"
+
+	test -s "$todo" ||
+	die "Aborted phase 2 of the ever-green rebase"
+
+	exit 0
+	;;
+esac
+
 # We need the following information:
 #
 # - the tip of the ever-green branch (HEAD)
@@ -92,4 +195,43 @@ then
 	die '`git rebase` failed to exec'
 fi
 
-die "TODO"
+# This function rebases the new changes on top of the ever-green branch
+pick_new_changes_onto_ever_green () {
+	ever_green_tip="$(git rev-parse --verify HEAD)" ||
+	die "Could not determine the tip of the ever-green (checked-out) branch"
+
+	echo "reset $ever_green_tip"
+	make_script "$current_tip" -ki --autosquash --rebase-merges=rebase-cousins --onto "$ever_green_tip" "$previous_tip"
+}
+
+replace_todo="$(git rev-parse --absolute-git-dir)/replace-todo"
+pick_new_changes_onto_ever_green >"$replace_todo" ||
+die "Could not generate todo list for $previous_tip..$current_tip"
+
+help="$(extract_todo_help "$replace_todo")" ||
+die "Could not extract todo help from $replace_todo"
+
+THIS_SCRIPT="$(realpath "$0")"
+test 0 = $(git rev-list --count "$ever_green_tip".."$onto") || {
+	cat >>"$replace_todo" <<-EOF
+
+	# Now perform the rebase onto upstream
+	exec "$THIS_SCRIPT" nested-rebase -kir --autosquash --onto "$onto" "$ever_green_base"
+	EOF
+}
+
+test -z "$help" ||
+echo "$help" >>"$replace_todo" ||
+die "Could not append rebase help text to $replace_todo"
+
+export ORIGINAL_GIT_SEQUENCE_EDITOR="$GIT_SEQUENCE_EDITOR"
+test -n "$ORIGINAL_GIT_SEQUENCE_EDITOR" || {
+	ORIGINAL_GIT_SEQUENCE_EDITOR="$(git config sequence.editor)"
+	test -n "$ORIGINAL_GIT_SEQUENCE_EDITOR" ||
+	ORIGINAL_GIT_SEQUENCE_EDITOR="$(git var GIT_EDITOR)"
+	test -n "$ORIGINAL_GIT_SEQUENCE_EDITOR" ||
+	die "Could not determine editor"
+}
+export GIT_SEQUENCE_EDITOR="\"$THIS_SCRIPT\" replace-todo-script"
+exec git rebase -kir HEAD ||
+die "Could not start the 2-phase rebase"
