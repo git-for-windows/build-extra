e1b76981cd7da7a669f327794121512c301c324d
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-04-01 14:42:38 +0200

    credential-helper-selector: implement the radio buttons to choose the helper
    The idea is that this list of credential helpers from which the user
    can pick one will be generated dynamically. We will do that later, for
    the moment we only implement the GUI part.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- git-extra/git-credential-helper-selector.c
+++ git-extra/git-credential-helper-selector.c
@@ -1,7 +1,32 @@
 #include <windows.h>
 
+static HINSTANCE instance;
+static LPWSTR *helper_name, *helper_path;
+static size_t helper_nr, selected_helper;
+
 #define ID_ENTER   IDOK
 #define ID_ABORT   IDCANCEL
+#define ID_USER    2000
+
+static int discover_helpers(void)
+{
+	/* TODO: walk `PATH` to populate these */
+	helper_nr = 2;
+	helper_name = malloc(helper_nr * sizeof(*helper_name));
+	helper_path = malloc(helper_nr * sizeof(*helper_path));
+	if (!helper_name || !helper_path) {
+		MessageBoxW(NULL, L"Out of memory!", L"Error", MB_OK);
+		return -1;
+	}
+	helper_name[0] = L"<no helper>";
+	helper_path[0] = L"<none>";
+	helper_name[1] = L"manager";
+	helper_path[1] = L"C:\\Program Files\\Git\\mingw64\\libexec\\git-core\\git-credential-manager.exe";
+
+	selected_helper = 1;
+
+	return 0;
+}
 
 static int aborted = 1;
 
@@ -16,14 +41,35 @@ static LRESULT CALLBACK window_proc(HWND hwnd, UINT message, WPARAM wParam,
 	case WM_CREATE: {
 		RECT rect;
 		int width;
+		size_t i;
 
 		GetClientRect(hwnd, &rect);
 		width = rect.right - rect.left;
+		CreateWindowW(L"Button", L"Select a credential helper",
+			      WS_CHILD | WS_VISIBLE | BS_GROUPBOX,
+			      offset_x, offset_y,
+			      width - 2 * offset_x,
+			      line_height * helper_nr + 3 * offset_y,
+			      hwnd, (HMENU) 0, instance, NULL);
+		for (i = 0; i < helper_nr; i++) {
+			HWND hwnd2 = CreateWindowW(L"Button", helper_name[i],
+						   WS_CHILD | WS_VISIBLE |
+						   BS_AUTORADIOBUTTON,
+						   2 * offset_x,
+						   3 * offset_y + line_height * i,
+						   width - 4 * offset_x,
+						   line_height + line_offset_y,
+						   hwnd, (HMENU)(ID_USER + i), instance, NULL);
+			if (i == selected_helper) {
+				SendMessage(hwnd2, BM_SETCHECK, BST_CHECKED, 1);
+				SetFocus(hwnd2);
+			}
+		}
 
 		CreateWindowW(L"Button", L"Select",
 			      WS_VISIBLE | WS_CHILD,
 			      width - 2 * (button_width + offset_x),
-			      offset_y,
+			      5 * offset_y + line_height * helper_nr,
 			      button_width,
 			      line_height + line_offset_y,
 			      hwnd, (HMENU) ID_ENTER, NULL, NULL);
@@ -31,7 +77,7 @@ static LRESULT CALLBACK window_proc(HWND hwnd, UINT message, WPARAM wParam,
 		CreateWindowW(L"Button", L"Cancel",
 			      WS_VISIBLE | WS_CHILD,
 			      width - (button_width + offset_x),
-			      offset_y,
+			      5 * offset_y + line_height * helper_nr,
 			      button_width,
 			      line_height + line_offset_y,
 			      hwnd, (HMENU) ID_ABORT, NULL, NULL);
@@ -44,6 +90,8 @@ static LRESULT CALLBACK window_proc(HWND hwnd, UINT message, WPARAM wParam,
 			SendMessage(hwnd, WM_CLOSE, 0, 0);
 		} else if (wParam == ID_ABORT) {
 			SendMessage(hwnd, WM_CLOSE, 0, 0);
+		} else if (HIWORD(wParam) == BN_CLICKED && LOWORD(wParam) >= ID_USER) {
+			selected_helper = LOWORD(wParam) - ID_USER;
 		}
 		break;
 
@@ -67,6 +115,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	window_class.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
 	window_class.hCursor = LoadCursor(0, IDC_ARROW);
 
+	if (discover_helpers() < 0) {
+		MessageBoxW(NULL, L"Could not discover credential helpers", L"Error", MB_OK);
+		return 1;
+	}
+
+	instance = hInstance;
 	RegisterClassW(&window_class);
 	CreateWindowW(window_class.lpszClassName, L"CredentialHelperSelector",
 		      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
