a166c9879cacb4bfe7d12487fa7ab0592e1c1178
Philip Oakley <philipoakley@iee.email>
2020-04-25 22:51:53 +0100

    Release Notes rendering: adjust collapsible arrow style and size
    Increase the font size of collapsible div's arrow and change them
    to an open triangle (White Right/Down Triangle U+25B7/D), hopefully
    to distinguish them from simple bullet markers.
    
    Also change the mouse over cursor to the pointing hand, ready for
    the user mouse click.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.email>

--- render-release-notes.sh
+++ render-release-notes.sh
@@ -126,21 +126,23 @@ render_release_notes () {
 				(() => {
 					for (let el of document.getElementsByClassName('collapsible')) {
 						let arrow = document.createElement('div');
-						arrow.innerHTML = '⯆';
+						arrow.innerHTML = '▽';
 						arrow.style.float = 'left';
 						arrow.style.position = 'relative';
 						arrow.style.left = '-1em';
 						arrow.style.top = '+1.5em';
+						arrow.style.fontSize = 'larger';
+						arrow.style.cursor = 'pointer';
 
 						const toggle = () => {
 							// this.classList.toggle('active');
 							let details = el.nextElementSibling;
 							if (details.style.display === 'none') {
 								details.style.display = 'block';
-								arrow.innerHTML = '⯆';
+								arrow.innerHTML = '▽';
 							} else {
 								details.style.display = 'none';
-								arrow.innerHTML = '⯈';
+								arrow.innerHTML = '▷';
 							}
 						};
 
