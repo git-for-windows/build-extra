53c1567c054e46d64a4025dc20ec2908aaa3f363
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-07-13 11:36:16 +0200

    please.sh create_sdk_artifacts: really accommodate Git v2.28.0
    The previous fix was actually not correct: `extensions.worktreeConfig`
    was already set. However, what needs to be changed is to set the
    repository format version appropriately to activate the `extensions.*`
    settings.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- please.sh
+++ please.sh
@@ -4337,10 +4337,10 @@ create_sdk_artifact () { # [--out=<directory>] [--git-sdk=<directory>] [--bitnes
 		return 0
 	}
 
+	git -C "$output_path" config core.repositoryFormatVersion 1 &&
 	git -C "$git_sdk_path" config extensions.worktreeConfig true &&
 	git -C "$git_sdk_path" worktree add --detach --no-checkout "$output_path" HEAD &&
 	sparse_checkout_file="$(git -C "$output_path" rev-parse --git-path info/sparse-checkout)" &&
-	git -C "$output_path" config extensions.worktreeConfig &&
 	git -C "$output_path" config --worktree core.sparseCheckout true &&
 	git -C "$output_path" config --worktree core.bare false &&
 	mkdir -p "${sparse_checkout_file%/*}" &&
