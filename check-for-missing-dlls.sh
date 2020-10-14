e1d623131e959922478a711cef2a27c3bbdbf633
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-10-14 20:25:37 +0200

    check-for-missing-dlls: work around i686-specific objdump problem
    Apparently the i686 version of objdump does not recognize the `.exe`
    files generated by `cv2pdb` as valid executables. But `ldd` does.
    
    So let's specifically fall back to using `ldd` for those (but do not use
    `ldd` by default because it hangs on other `.exe` files...).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- check-for-missing-dlls.sh
+++ check-for-missing-dlls.sh
@@ -31,7 +31,8 @@ else
 fi
 
 used_dlls_file=/tmp/used-dlls.$$.txt
-trap "rm \"$used_dlls_file\"" EXIT
+tmp_file=/tmp/tmp.$$.txt
+trap "rm \"$used_dlls_file\" \"$tmp_file\"" EXIT
 
 all_files="$(export ARCH BITNESS && "$thisdir"/make-file-list.sh | tr A-Z a-z)" &&
 usr_bin_dlls="$(echo "$all_files" | grep '^usr/bin/[^/]*\.dll$')" &&
@@ -49,7 +50,11 @@ do
 
 	paths=$(echo "$all_files" |
 		sed -ne 's,[][],\\&,g' -e "s,^$dir[^/]*\.\(dll\|exe\)$,/&,p")
-	out="$(/usr/bin/objdump -p $paths)"
+	out="$(/usr/bin/objdump -p $paths 2>"$tmp_file")"
+	paths="$(sed -n 's|^/usr/bin/objdump: \([^ :]*\): file format not recognized|\1|p' <"$tmp_file")"
+	test -z "$paths" ||
+	out="$out$LF$(ldd $paths)"
+
 	echo "$out" |
 	tr A-Z\\r a-z\  |
 	grep -e '^.dll name:' -e '^[^ ]*\.\(dll\|exe\):' -e '\.dll =>' |
