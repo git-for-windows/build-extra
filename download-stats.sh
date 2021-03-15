4dfcae554dd036efd81fe06ec7c8305305a45c91
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-03-15 22:05:01 +0000

    download-stats: new Git for Windows version
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- download-stats.sh
+++ download-stats.sh
@@ -20,12 +20,6 @@ test "--update" != "$1" || {
 }
 
 # IDs
-# v2.25.0.windows.3
-#id=${1:-25701890}
-# v2.23.0.windows.5
-#id=${1:-25851771}
-# v2.25.0.windows.4
-#id=${1:-25851864}
 # v2.21.0.windows.7
 #id=${1:-25851954}
 # v2.14.4.windows.8
@@ -79,7 +73,13 @@ test "--update" != "$1" || {
 # v2.31.0-rc1.windows.1
 #id=${1:-39205162}
 # v2.30.2.windows.1
-id=${1:-39534296}
+#id=${1:-39534296}
+# v2.29.2.windows.4
+#id=${1:-39539472}
+# v2.31.0-rc2.windows.1
+#id=${1:-39614982}
+# v2.31.0.windows.1
+id=${1:-39845515}
 # v2.11.1.mingit-prerelease.6
 # v2.11.1.mingit-prerelease.4
 # v2.11.1.mingit-prerelease.5
