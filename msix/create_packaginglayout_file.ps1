3897639f2c2ee8065a5b8a527a4b52b249c9408e
David Anthoff <anthoff@berkeley.edu>
2021-07-19 00:41:20 -0700

    Fix packaging layout file

--- msix/create_packaginglayout_file.ps1
+++ msix/create_packaginglayout_file.ps1
@@ -5,21 +5,21 @@ $packageLayout = [xml]@"
   <PackageFamily ID="Git-$($version)" FlatBundle="false" ManifestPath="appxmanifest.xml" ResourceManager="false">
     <Package ID="Git-x64-$($version)" ProcessorArchitecture="x64">
       <Files>
-        <File DestinationPath="Git\*" SourcePath="x64\*" />
+        <File DestinationPath="Git\*" SourcePath="x64\**" />
         <File DestinationPath="Images\*.png" SourcePath="..\build-extra\msix\Images\*.png" />
         <File DestinationPath="Public\Fragments\*" SourcePath="..\build-extra\msix\Fragments\*" />
       </Files>
     </Package>
     <Package ID="Git-x86-$($version)" ProcessorArchitecture="x86">
       <Files>
-        <File DestinationPath="Git\*" SourcePath="x86\*" />
+        <File DestinationPath="Git\*" SourcePath="x86\**" />
         <File DestinationPath="Images\*.png" SourcePath="..\build-extra\msix\Images\*.png" />
         <File DestinationPath="Public\Fragments\*" SourcePath="..\build-extra\msix\Fragments\*" />
       </Files>
     </Package>
     <Package ID="Git-ARM64-$($version)" ProcessorArchitecture="ARM64">
       <Files>
-        <File DestinationPath="Git\*" SourcePath="arm64\*" />
+        <File DestinationPath="Git\*" SourcePath="arm64\**" />
         <File DestinationPath="Images\*.png" SourcePath="..\build-extra\msix\Images\*.png" />
         <File DestinationPath="Public\Fragments\*" SourcePath="..\build-extra\msix\Fragments\*" />
       </Files>
