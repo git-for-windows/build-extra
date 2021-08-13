f108b173d76286c2a488aee515e863c169cd1ff0
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-08-13 09:58:29 +0200

    please.sh build_mingw_w64_git: allow multiple tags to point at HEAD
    If multiple tags point to HEAD, just use the first one.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- please.sh
+++ please.sh
@@ -4814,7 +4814,7 @@ build_mingw_w64_git () { # [--only-32-bit] [--only-64-bit] [--skip-test-artifact
 	git clone --depth 1 --single-branch -b main https://github.com/git-for-windows/MINGW-packages /usr/src/MINGW-packages ||
 	die "Could not clone MINGW-packages\n"
 
-	tag="$(git for-each-ref --format '%(refname:short)' --points-at="${1:-HEAD}" 'refs/tags/v[0-9]*')"
+	tag="$(git for-each-ref --count=1 --format '%(refname:short)' --points-at="${1:-HEAD}" 'refs/tags/v[0-9]*')"
 	test -n "$tag" || {
 		tag=$(git describe --match=v* "${1:-HEAD}" | sed 's/-.*//').$(date +%Y%m%d%H%M%S) &&
 		git tag $tag "${1:-HEAD}"
