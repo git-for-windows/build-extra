e011309aeb8cfc7a4f7e4b58c0e453a8a9a3c4c7
David Refoua <David@Refoua.me>
2023-01-30 11:29:26 +0330

    remove duplicated line, move change color line into if statement
    Signed-off-by: David Refoua <David@Refoua.me>

--- git-extra/git-prompt.sh
+++ git-extra/git-prompt.sh
@@ -13,11 +13,10 @@ else
 	PS1="$PS1"'\n'                 # new line
 	PS1="$PS1"'\[\033[32m\]'       # change to green
 	PS1="$PS1"'\u@\h '             # user@host<space>
-	PS1="$PS1"'\[\033[35m\]'       # change to purple
-	PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
 	if [ ! -z "$MSYSTEM" ]
 	then
-		PS1="$PS1"'$MSYSTEM '  # show MSYSTEM
+		PS1="$PS1"'\[\033[35m\]'        # change to purple
+		PS1="$PS1"'$MSYSTEM '           # show MSYSTEM
 	fi
 	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
 	PS1="$PS1"'\w'                 # current working directory
