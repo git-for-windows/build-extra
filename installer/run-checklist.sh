cc07eca28e30469ef2885b171af0d44f532a231f
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-05-17 08:58:29 +0200

    installer: adjust pre-flight checklist for the libcurl split once more
    The adjustment in d8a6893f8 (installer: adjust pre-flight checklist for
    the libcurl split, 2023-05-15) was too tight; The `git-artifacts`
    workflows of the `git-sdk-*` repositories use `git.exe` built from
    `git-for-windows/git`'s `main` branch. That means that the output of
    their `git.exe version` will look something like `git version
    2.40.1.windows.1.<stuff-and-things>`.
    
    Let's loosen that check.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- installer/run-checklist.sh
+++ installer/run-checklist.sh
@@ -12,7 +12,11 @@ esac
 
 git -c http.sslbackend=schannel ls-remote https://github.com/dscho/images
 git -c http.sslbackend=openssl ls-remote https://github.com/dscho/images ||
-test "git version 2.40.1.windows.1" = "$(git version)"
+# support for http.sslBackend was introduced in v2.13.1, but it requires
+# mingw-w64-curl, whereas Git for Windows v2.41.0 switched to the combination
+# of mingw-w64-curl-winssl and mingw-w64-curl-openssl-alternate (which earlier
+# `git.exe` versions cannot handle)
+case "$(git version)" in *2.40.*|*2.[123][0-9].*) true;; *) exit 123;; esac
 
 git ls-remote git@ssh.dev.azure.com:v3/git-for-windows/git/git main
 
