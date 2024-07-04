git-for-windows/build-extra

commit ff3a9068f31ef3340a0bb8f501af3628c5193c8f

mingit: exclude new sshd executable
The `sshd.exe` file is already excluded from MinGit for quite a while.
As of OpenSSH 9.8, there is a new `sshd-session.exe`, which also needs
to be excluded.

This new file is part of an ongoing effort to split `sshd` further, see
https://github.com/openssh/openssh-portable/commit/03e3de416ed7c34faeb692967737be4a7bbe2eb5
for more details.

As a consequence, let's not just exclude `sshd-session.exe` piecemeal
fashion, but all `sshd-*.exe` files, whatever they be in the future.

This fixes the `check-for-missing-dlls` run that fails currently, see
https://github.com/git-for-windows/git-sdk-64/actions/runs/9754263596/job/26920893577#step:6:9
for an example.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

make-file-list.sh
@@ -326,7 +326,7 @@ else
 		-e '^/usr/bin/\(pr\|printenv\|ps\|ptx\|realpath\)\.exe$' \
 		-e '^/usr/bin/\(regtool\|runcon\|scp\|seq\|setfacl\)\.exe$' \
 		-e '^/usr/bin/\(setmetamode\|sftp\|sha.*sum\|shred\)\.exe$' \
-		-e '^/usr/bin/\(shuf\|sleep\|slogin\|split\|sshd\)\.exe$' \
+		-e '^/usr/bin/\(shuf\|sleep\|slogin\|split\|sshd)\.exe$' \
 		-e '^/usr/bin/\(ssh-key.*\|ssp\|stat\|stdbuf\|strace\)\.exe$' \
 		-e '^/usr/bin/\(stty\|sum\|sync\|tac\|tee\|timeout\)\.exe$' \
 		-e '^/usr/bin/\(truncate\|tsort\|tty\|tzset\|umount\)\.exe$' \
@@ -335,7 +335,7 @@ else
 		-e '^/usr/bin/msys-sqlite3[a-z].*\.dll$' \
 		-e '^/usr/bin/msys-\(gomp.*\|vtv.*\)-.*\.dll$' \
 		-e '^/usr/lib/\(awk\|coreutils\|gawk\|openssl\|pkcs11\)/' \
-		-e '^/usr/lib/ssh/sftp' \
+		-e '^/usr/lib/ssh/\(sftp\|sshd\($\|-\)\)' \
 		-e '^/usr/libexec/\(bigram\|code\|frcode\)\.exe$' \
 		-e '^/usr/share/\(cygwin\|git\)/' \
 		-e '^/usr/ssl/misc/' \

commit 151844d0279677adeac28695cdf30c54a5d8abb0

mingit: fix a regression
In ff3a9068f (mingit: exclude new sshd executable, 2024-07-02), I
inadvertently dropped a backslash in a crucial place. The symptom is
that `make-file-list.sh` now prints the error message:

	grep: Unmatched ( or \(

and the `mingit/release.sh` script complains that:

	Could not find any .exe files in libexec/git-core/

Add the backslash to address both symptoms in one fell swoop.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

make-file-list.sh
@@ -326,7 +326,7 @@ else
 		-e '^/usr/bin/\(pr\|printenv\|ps\|ptx\|realpath\)\.exe$' \
 		-e '^/usr/bin/\(regtool\|runcon\|scp\|seq\|setfacl\)\.exe$' \
 		-e '^/usr/bin/\(setmetamode\|sftp\|sha.*sum\|shred\)\.exe$' \
-		-e '^/usr/bin/\(shuf\|sleep\|slogin\|split\|sshd)\.exe$' \
+		-e '^/usr/bin/\(shuf\|sleep\|slogin\|split\|sshd\)\.exe$' \
 		-e '^/usr/bin/\(ssh-key.*\|ssp\|stat\|stdbuf\|strace\)\.exe$' \
 		-e '^/usr/bin/\(stty\|sum\|sync\|tac\|tee\|timeout\)\.exe$' \
 		-e '^/usr/bin/\(truncate\|tsort\|tty\|tzset\|umount\)\.exe$' \