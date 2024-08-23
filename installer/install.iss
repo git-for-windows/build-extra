git-for-windows/build-extra

commit 490887923bb7a1d312ab217ac3bd7140b4a4c45f

installer: be prepared for Git tags when comparing versions
A Git tag has a slightly different format from the one
`VersionCompare()` currently expects: A Git for Windows tag usually has
the `.windows.<patch-level>` suffix, and Git versions can have the
`.rc<N>` suffix.

Let's just skip identical non-numerical substrings when comparing
versions.

This also fixes the (theoretical) bug where `2.19a` and `2.19b` would
have been treated as identical versions.

Note: to allow for comparing, say, `v2.24.0.rc1.windows.1` to
`v2.24.0.windows.1`, we willfully rely on the fact that `r` has a lower
numerical value in ASCII than `w`.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

installer/install.iss
@@ -816,6 +816,13 @@ begin
         if Previous<0 then begin
             if Current>=0 then
                 Result:=+1;
+            Result:=Ord(CurrentVersion[i])-Ord(PreviousVersion[j]);
+            if (Result=0) then begin
+                // skip identical non-numerical characters
+                i:=i+1;
+                j:=j+1;
+                Continue;
+            end;
             Exit;
         end;
         if Current<0 then begin

commit 6973873071adcd78a4aded2506ab3900c7f7dbe1

installer: fix version comparison bug
This is a left-over from long ago, introduced in 490887923b (installer:
be prepared for Git tags when comparing versions, 2019-10-30).
Reconstructing my memory from that commit, it looks as if my intention
was to replace the faulty logic, but I left in that now-useless
conditional assignment of `Result` which is immediately overridden.

To help future me, let's just remove the unneeded code.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

installer/install.iss
@@ -952,8 +952,6 @@ begin
         Previous:=NextNumber(PreviousVersion,j);
         Current:=NextNumber(CurrentVersion,i);
         if Previous<0 then begin
-            if Current>=0 then
-                Result:=+1;
             Result:=Ord(CurrentVersion[i])-Ord(PreviousVersion[j]);
             if (Result=0) then begin
                 // skip identical non-numerical characters