git-for-windows/build-extra

commit c0fb1e52759b873e14876ccbaf42745c087abe3b

installer: re-generate the system config instead of copying it
When Git for Windows is shipped as part of a bigger system (such as VFS
for Git), it can make a ton of sense to set system config variables. In
such a scenario, it is really, really unfortunate that Git for Windows'
installer simply blows it away and copies its own version over it during
an upgrade.

To help with that, let's change the logic. Let's teach the installer to
work on an existing system config (if any), setting the system config
values explicitly via `git config calls`.

Note: this does not yet save the current system config when upgrading
Git for Windows; That task is left for another commit.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

installer/install.iss
@@ -2238,10 +2238,6 @@ begin
     if FileExists(ProgramData+'\Git\config') then begin
         if not Exec(AppDir+'\bin\bash.exe','-c "value=\"$(git config -f config pack.packsizelimit)\" && if test 2g = \"$value\"; then git config -f config --unset pack.packsizelimit; fi"',ProgramData+'\Git',SW_HIDE,ewWaitUntilTerminated,i) then
             LogError('Unable to remove packsize limit from ProgramData config');
-#if BITNESS=='32'
-        if not Exec(AppDir+'\{#MINGW_BITNESS}\bin\git.exe','config --system pack.packsizelimit 2g',AppDir,SW_HIDE,ewWaitUntilTerminated,i) then
-            LogError('Unable to limit packsize to 2GB');
-#endif
         Cmd:=AppDir+'/';
         StringChangeEx(Cmd,'\','/',True);
         if not Exec(AppDir+'\bin\bash.exe','-c "value=\"$(git config -f config http.sslcainfo)\" && case \"$value\" in \"'+Cmd+'\"/*|\"C:/Program Files/Git/\"*|\"c:/Program Files/Git/\"*) git config -f config --unset http.sslcainfo;; esac"',ProgramData+'\Git',SW_HIDE,ewWaitUntilTerminated,i) then
@@ -2463,8 +2459,6 @@ begin
     }
 
     if not IsComponentSelected('gitlfs') then begin
-        if not Exec(AppDir + '\{#MINGW_BITNESS}\bin\git.exe','config --system --remove-section filter.lfs','',SW_HIDE,ewWaitUntilTerminated, i) then
-            LogError('Could not disable Git LFS in the gitconfig.');
         if not DeleteFile(AppDir+'\{#MINGW_BITNESS}\bin\git-lfs.exe') and not DeleteFile(AppDir+'\{#MINGW_BITNESS}\libexec\git-core\git-lfs.exe') then
             LogError('Line {#__LINE__}: Unable to delete "git-lfs.exe".');
     end;

installer/release.sh
@@ -150,28 +150,24 @@ esac
 GITCONFIG_PATH="$(echo "$LIST" | grep "^mingw$BITNESS/etc/gitconfig\$")"
 printf '' >programdata-config.template
 test -z "$GITCONFIG_PATH" || {
-	cp "/$GITCONFIG_PATH" gitconfig.system &&
 	cp "/$GITCONFIG_PATH" programdata-config.template &&
-	keys="$(git config -f gitconfig.system -l --name-only)" &&
+	keys="$(git config -f programdata-config.template -l --name-only)" &&
+	gitconfig="$LF[Code]${LF}function GitSystemConfigSet(Key,Value:String):Boolean; forward;$LF" &&
+	gitconfig="$gitconfig${LF}function SetSystemConfigDefaults():Boolean;${LF}begin${LF}    Result:=True;${LF}" &&
 	for key in $keys
 	do
 		case "$key" in
-		pack.packsizelimit)
-			# remove from both, will be configured by installer
-			git config -f programdata-config.template \
-				--unset "$key" &&
-			git config -f gitconfig.system --unset "$key" ||
-			break
-			;;
-		diff.astextplain.*|filter.lfs.*|http.sslcainfo)
-			# keep in the system-wide config
-			git config -f programdata-config.template \
-				--unset "$key" ||
-			break
-			;;
-		*)
-			# move to the ProgramData template
-			git config -f gitconfig.system --unset "$key" ||
+		pack.packsizelimit|diff.astextplain.*|filter.lfs.*|http.sslcainfo)
+			# set in the system-wide config
+			value="$(git config -f programdata-config.template "$key")" &&
+			case "$key$value" in *"'"*) die "Cannot handle $key=$value because of the single quote";; esac &&
+			git config -f programdata-config.template --unset "$key" &&
+			case "$key" in
+			filter.lfs.*) extra=" IsComponentSelected('gitlfs') And";;
+			pack.packsizelimit) test $BITNESS = 32 || continue; value=2g;;
+			*) extra=;;
+			esac &&
+			gitconfig="$gitconfig$LF    if$extra not GitSystemConfigSet('$key','$value') then$LF        Result:=False;" ||
 			break
 			;;
 		esac || break
@@ -180,13 +176,14 @@ test -z "$GITCONFIG_PATH" || {
 	sed -i '/^\[/{:1;$d;N;/^.[^[]*$/b;s/^.*\[/[/;b1}' \
 		programdata-config.template ||
 	die "Could not split gitconfig"
+
+	gitconfig="$gitconfig${LF}end;$LF"
+	inno_defines="$inno_defines$LF$gitconfig"
+
 	LIST="$(echo "$LIST" | grep -v "^$GITCONFIG_PATH\$")"
 }
 
-printf '%s%s%s\n%s\n' \
-	'Source: {#SourcePath}\gitconfig.system; DestName: gitconfig; ' \
-	  "DestDir: {app}\\mingw$BITNESS\\etc; Flags: replacesameversion restartreplace; " \
-	  'AfterInstall: DeleteFromVirtualStore' \
+printf '%s\n' \
 	'Source: {#SourcePath}\programdata-config.template; Flags: dontcopy' \
 	>>file-list.iss ||
 die "Could not append gitconfig to file list"

commit 7cdcad981e934e61d2cce6092c8a1b96ae8426db

installer: do remove the system config when uninstalling
When uninstalling Git for Windows, `gitconfig` remains, as pointed out in
https://github.com/git-for-windows/git/issues/5492#issuecomment-2733104264

The reason for this is a regression I introduced in c0fb1e5275
(installer: re-generate the system config instead of copying it,
2019-02-19): before that change, `/etc/gitconfig` was copied regularly,
and InnoSetup would of course know to remove it as part of uninstalling
the software. After that change, however, the system config is
(re-)generated dynamically, and InnoSetup does not know that it needs to
be removed when uninstalling.

Let's tell InnoSetup that it should delete that file when uninstalling.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

installer/install.iss
@@ -278,8 +278,9 @@ Type: dirifempty; Name: {app}\{#MINGW_BITNESS}\libexec
 Type: dirifempty; Name: {app}\{#MINGW_BITNESS}
 Type: dirifempty; Name: {app}
 
-; Delete Git Bash options
+; Delete Git Bash options and the system config
 Type: files; Name: {app}\etc\git-bash.config
+Type: files; Name: {app}\etc\gitconfig
 Type: dirifempty; Name: {app}\etc
 
 ; Delete Windows Terminal profile fragments