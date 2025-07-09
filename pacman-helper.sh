5320194430e09753800ef6c6d1a232bf644afa44
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-07-09 15:12:39 +0200

    pacman-helper: make sure that the `.git/info/` directory exists
    ... before trying to write to it...
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- pacman-helper.sh
+++ pacman-helper.sh
@@ -224,6 +224,7 @@ quick_action () { # <action> <file>...
 	git -C "$dir" config set remote.origin.partialCloneFilter blob:none &&
 	git -C "$dir" config set core.sparseCheckout true &&
 	git -C "$dir" config set core.sparseCheckoutCone false &&
+	mkdir -p "$dir"/.git/info &&
 	printf '%s\n' '/git-*.db*' '/git-*.files*' >"$dir"/.git/info/sparse-checkout &&
 	printf '%s\n' '/git-for-windows.db*' '/git-for-windows.files*' >"$dir"/.git/info/exclude &&
 	mkdir "$dir/sources" ||
